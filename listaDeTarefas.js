ee0bdf12177ff721586c50f118c9ef0dcb9ba8ed
renatopl97 <renatopl97@gmail.com>
2022-11-15 16:32:39 -0300

    finalizado a função de adicionar a tarefa

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,11 +1,19 @@
 const criarTarefa = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-    console.log(valor);
-    input.value = " ";
+    evento.preventDefault(); //impede o recarregamento da pagina 
+
+    const lista = document.querySelector("[data-list]"); //seleciona a ul onde vai ser adicionada as anotações
+    const input = document.querySelector('[data-form-input]'); //seleciona a caixa de texto/form
+    const valor = input.value; //obtém o "valor" digitado
+
+    const tarefa = document.createElement('li'); //const que cria a li
+    tarefa.classList.add('task') //adiciona a class ao li
+    const conteudo = `<p class="content">${valor}</p>`; //const que cria o paragrafo com o texto digitado
+
+    tarefa.innerHTML = conteudo; //insere no html o paragrafo 
+    lista.appendChild(tarefa); //adicionar o paragrafo como filho da li
+    input.value = " "; //limpa o texto digitado no form
 }
 
-const novaTarefa = document.querySelector('[data-form-button]');
+const novaTarefa = document.querySelector('[data-form-button]'); //selecionando o botão
 
-novaTarefa.addEventListener('click', criarTarefa);
+novaTarefa.addEventListener('click', criarTarefa); //aqui vai usar o Criar tarefa ao clicar no botão
